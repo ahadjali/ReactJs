1b293e3892726b9afe41456d8a42c61b8c81fa91
Anis El Hadj Ali <elhadjalianis@gmail.com>
2021-09-18 17:56:48 +0100

    ReactJS_V_2.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import './style.css';
+import imageInSrc from './imageInSrc.jpg';
+import myVideo from './myVideo.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: '100vw' }}>
+        <h1 class="title red">Anis</h1>
+        <br />
+        <img src={imageInSrc} alt='imageInSrc' />
+        <br />
+        <img src="imageInPublic.jpg" alt='imageInPublic' />
+      </div>
+      <video width="320" height="240" controls>
+        <source src={myVideo} type="video/mp4" />
+      </video>
     </div>
   );
 }
